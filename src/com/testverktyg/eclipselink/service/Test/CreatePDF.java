fefa1ee4cf5d64cef7f0862611f8f6500dcf3234
Daniel <axhammar.daniel@gmail.com>
2017-05-29 15:46:45 +0200

    Update CreatePDF Class
    Uppdaterad klas, mer kommer att göras senare!

--- src/com/testverktyg/eclipselink/service/Test/CreatePDF.java
+++ src/com/testverktyg/eclipselink/service/Test/CreatePDF.java
@@ -3,20 +3,19 @@ package com.testverktyg.eclipselink.service.Test;
 
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfWriter;
-import com.testverktyg.eclipselink.entity.Test;
 import com.testverktyg.eclipselink.entity.User;
-import org.eclipse.persistence.jpa.jpql.parser.DateTime;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-
 
 /**
  * Created by Daniel on 2017-05-24.
  */
+
 public class CreatePDF {
 
     public static String DEST = "TestPDF";
@@ -28,32 +27,29 @@ public class CreatePDF {
 
     public static void main(String[] args) throws DocumentException, IOException
     {
-        new CreatePDF().createPDF(DEST);
-
+        new CreatePDF().createTeacherAndAdminPDF(DEST);
+        new CreatePDF().createStudentPDF(DEST);
 
     }
 
-    private void createPDF(String dest) throws DocumentException, IOException{
+    private void createStudentPDF(String dest)throws DocumentException {
 
         EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("Eclipselink_JPA");
         EntityManager entitymanager = emfactory.createEntityManager();
         entitymanager.getTransaction().begin();
 
-        User user = entitymanager.find(User.class, 4); // get user who has id 19.
-        Test test = entitymanager.find(Test.class,userId);
-        firstname = user.getFirstname();
-        lastname = user.getLastname();
+        User user = entitymanager.find(User.class, userId);
 
+        Document document = new Document();
 
-        System.out.println(firstname + " " + lastname);
-
-
-
-        Document document = new Document(PageSize.A4);
-
-        PdfWriter.getInstance(document, new FileOutputStream("testpdf.pdf"));
+        try {
+            PdfWriter.getInstance(document, new FileOutputStream("testStudentpdf.pdf"));
+            System.out.println("testing 1");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         document.open();
-        System.out.println(firstname + " " + lastname);
+        System.out.println("testing 2");
         Font font = new Font(Font.FontFamily.HELVETICA, 14, Font.BOLD);
         Paragraph headline = new Paragraph("Student Test Score", font );
         headline.setAlignment(Element.ALIGN_CENTER);
@@ -61,13 +57,77 @@ public class CreatePDF {
         Paragraph emptySpace = new Paragraph(" ");
         document.add(emptySpace);
         document.add(emptySpace);
-        document.add(new Paragraph(firstname+ " " + lastname)); // whys dosent this works?
-        System.out.println("pdf has been created!");
+        document.add(new Paragraph(firstname+ " " + lastname));
+        Paragraph klass = new Paragraph("Klass: ");
+        document.add(klass);
+        Paragraph numbGquestion = new Paragraph("Antal Godkänd frågor:");
+        document.add(numbGquestion);
+        Paragraph numbVGquestion = new Paragraph("Antal Väl godkänd frågor:");
+        document.add(numbVGquestion);
+
+
+        document.close();
+    }
+
+    private void createTeacherAndAdminPDF(String dest) throws DocumentException, IOException{
+
+        EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("Eclipselink_JPA");
+        EntityManager entitymanager = emfactory.createEntityManager();
+        entitymanager.getTransaction().begin();
+
+        User user = entitymanager.find(User.class, 4); // get user who has id 19.
+        firstname = user.getFirstname();
+        lastname = user.getLastname();
+
+
+        System.out.println(firstname + " " + lastname);
+
+        Document document = new Document(PageSize.A4);
+
+        PdfWriter.getInstance(document, new FileOutputStream("testpdf.pdf"));
+        document.open();
+        Font font = new Font(Font.FontFamily.HELVETICA, 14, Font.BOLD);
+        Paragraph headline = new Paragraph("Student Test Score", font );
+        headline.setAlignment(Element.ALIGN_CENTER);
+        document.add(headline);
+        Paragraph emptySpace = new Paragraph(" ");
+        document.add(emptySpace);
+        document.add(emptySpace);
+        document.add(new Paragraph(firstname+ " " + lastname));
+        Paragraph testName = new Paragraph("Test:");
+        document.add(testName);
+        Paragraph numbGquestion = new Paragraph("Antal Godkänd frågor:");
+        document.add(numbGquestion);
+        Paragraph numbVGquestion = new Paragraph("Antal Väl godkänd frågor:");
+        document.add(numbVGquestion);
+        Paragraph testTime = new Paragraph("Tid:");
+        document.add(testTime);
+        Paragraph averageScore = new Paragraph("Snittresultat:");
+        document.add(averageScore);
+        Paragraph maxScore = new Paragraph("maxpoäng:");
+        document.add(maxScore);
+        Paragraph numbOfStudentsTakenTest = new Paragraph("Antal studenter som gjort test:");
+        document.add(numbOfStudentsTakenTest);
+        Paragraph numbOfFail = new Paragraph("Antal IG:");
+        document.add(numbOfFail);
+        Paragraph numbOfPass = new Paragraph("Antal G:");
+        document.add(numbOfPass);
+        Paragraph numbOfHighPass = new Paragraph("Antal VG:");
+        document.add(numbOfHighPass);
+
+
+
 
         document.close();
 
+    }
+
+    public void CreateStudentPDF(){
+
+
 
     }
+
 
 
     public int getUserId() {
